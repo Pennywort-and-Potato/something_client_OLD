ef160dff4722783b89d23cb138b893a78b02f10a
Đỗ Lê Duy <doleduy1999@gmail.com>
2023-01-17 11:59:04 +0700

    clear view

---
+++ src/layout/MainLayout/index.tsx
@@ -0,0 +1,56 @@
+import { Layout } from 'antd';
+import React from 'react'
+import Image from 'next/image';
+import style from '../MainLayout/style.module.css'
+import Link from 'next/link';
+
+interface data {
+  content: any;
+  props?: any;
+}
+
+const { Header, Content, Footer } = Layout;
+
+const layoutStyle = {
+  minHeight: '100vh'
+}
+
+const footerStyle = {
+  backgroundColor: '#001529',
+  color: 'rgb(82 200 235)',
+}
+
+const contentStyle = {
+  backgroundColor: '#fff'
+}
+
+function MainLayout(data: data) {
+
+  const { content, props } = data 
+
+  return (
+    <Layout style={layoutStyle}>
+      <Header>
+        <div className={style.header}>
+          <Link href='/' style={{ display: 'flex' }}>
+            <Image 
+              className={style.icon}
+              src='/icon.png' alt='' 
+              width={30} 
+              height={30}
+            />
+          </Link>
+        </div>
+      </Header>
+      <Content style={contentStyle}>
+        {content}
+      </Content>
+      <Footer style={footerStyle}>
+        @First credit - Tue 17 Jan 2023
+      </Footer>
+    </Layout>
+  )
+}
+
+
+export default MainLayout
