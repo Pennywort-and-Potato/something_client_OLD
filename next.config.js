ea47fb17c8304b0e245834c86650424b4e636a09
Nguyễn Bá Khoa <khoa.nguyenba@outlook.com>
2023-01-24 15:48:15 +0700

    Update next.config.js
    MAP /api to PORT 4000

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api',
+        destination: 'localhost:4000/',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
