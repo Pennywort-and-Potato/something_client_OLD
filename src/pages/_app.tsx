628e38296d3c49bdc77e7483caa37971db7838a4
Đỗ Lê Duy <doleduy1999@gmail.com>
2023-01-27 17:11:27 +0700

    update file struct

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
 import MainLayout from '../layouts/MainLayout'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
 
@@ -8,5 +9,12 @@ export default function App({ Component, pageProps }: AppProps) {
     content: <Component {...pageProps} />
   }
 
-  return <MainLayout {...data} />
+  return <>
+    <Head>
+      <title>Pennywort & Potato</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/icon.png" />
+    </Head>
+    <MainLayout {...data} />
+  </>
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,5 @@
-import Head from 'next/head'
 import styles from '@/styles/Home.module.css'
-import MainLayout from '../layouts/MainLayout'
 import { useEffect } from 'react'
-
-function renderContent() {
-  return (
-    <>
-    </>
-  )
-}
 
 function Home() {
 
@@ -16,18 +7,9 @@ function Home() {
     console.log('render')
   }, [])
 
-  const headerConfig = (
-    <Head>
-      <title>Pennywort & Potato</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="/icon.png" />
-    </Head>
-  )
-
   return (
     <>
-      {headerConfig}
-      <div>abc</div>
+      <div>Content</div>
     </>
   )
 }
