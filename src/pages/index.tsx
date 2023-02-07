40390fdbaf5a25a8ccfd058dd8e1ed653537c1e0
VirtueSea <duchaik20@gmail.com>
2023-02-07 08:56:20 +0700

    save work

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,18 @@
-import styles from '@/styles/Home.module.css'
-import { useEffect } from 'react'
+import styles from '@/styles/Home.module.css';
+import { useEffect, useRef } from 'react';
+import { Button, Carousel, Col, Divider, Row } from 'antd';
+
+
+const contentStyle: React.CSSProperties = {
+  height: '160px',
+  color: '#fff',
+  lineHeight: '160px',
+  textAlign: 'center',
+  background: '#364d79',
+};
+const style: React.CSSProperties = { background: '#0092ff', padding: '8px 0' };
+
+
 
 function Home() {
 
@@ -7,9 +20,96 @@ function Home() {
     console.log('render')
   }, [])
 
+  const ref = useRef()
+
   return (
     <>
-      <div>Content</div>
+    <div >
+      <h2 className={styles.title_banner}>Banner Slider</h2>
+     
+      <Carousel 
+        autoplay
+        effect="scrollx"
+        arrows
+        dots={true}
+        pauseOnHover={true}
+        pauseOnDotsHover={true}
+        dotPosition="bottom"
+        draggable
+        ref={ref}
+      >
+        {/* <div>
+          <h3 style={contentStyle}>1</h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>2</h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>3</h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>4</h3>
+        </div> */}
+        <div>
+          <Row gutter={16}>
+            <Col className="gutter-row" span={12}>
+            <div>
+              <h3 style={contentStyle}>1</h3>
+            </div>
+            </Col>
+            <Col className="gutter-row" span={12}>
+            <div>
+              <h3 style={contentStyle}>2</h3>
+            </div>
+            </Col>
+          </Row>
+        </div>
+        <div>
+          <Row gutter={16}>
+            <Col className="gutter-row" span={12}>
+            <div>
+              <h3 style={contentStyle}>3</h3>
+            </div>
+            </Col>
+            <Col className="gutter-row" span={12}>
+            <div>
+              <h3 style={contentStyle}>4</h3>
+            </div>
+            </Col>
+          </Row> 
+        </div>
+        
+      </Carousel>
+
+      <div>
+        <Button onClick={() => {
+            ref.current.prev()
+        }}>Prev</Button>
+        <Button onClick={() => {
+            ref.current.goTo(0)
+        }}>Reset</Button>
+        <Button onClick={() => {
+            ref.current.next()
+        }}>Next</Button>
+      </div> 
+    </div>
+    <div >
+      <h2 className={styles.title_banner}>Grid image</h2>
+      <Divider orientation="left">Horizontal</Divider>
+      <Row gutter={6}>
+        <Col className="gutter-row" span={8}>
+          <div style={style}>col-8</div>
+        </Col>
+        <Col className="gutter-row" span={8}>
+          <div style={style}>col-8</div>
+        </Col>
+        <Col className="gutter-row" span={8}>
+          <div style={style}>col-8</div>
+        </Col>
+        
+      </Row>
+    </div>
+      
     </>
   )
 }
