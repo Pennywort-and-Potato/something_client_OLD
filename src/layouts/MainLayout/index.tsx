9f13afbef66f59ee956661c4962845a65bfa0154
saitato <gchieu280598@gmail.com>
2023-02-08 14:39:13 +0700

    hieu up code xin

--- src/layouts/MainLayout/index.tsx
+++ src/layouts/MainLayout/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import style from '../MainLayout/style.module.css'
 import Footer from './Footer';
 import Header from './Header';
@@ -10,11 +10,16 @@ interface props {
 function MainLayout(props: props) {
 
   const { content } = props
+  const [ theme, setTheme ] = useState(false)
+
+  const callback = (el: boolean) => {
+    setTheme(el)
+  }
   
   return (
     <>
-      <div className={style.container}>
-        <Header />
+      <div className={`${style.container} ${theme ? 'l-dark' : 'l-light'}`} >
+        <Header changeTheme={callback}  />
         <div className={style.body}>
           {content}
         </div>
